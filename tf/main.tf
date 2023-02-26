e2662648ad3ff9d48333a5efbdec9ee288a80e20
mfranz <franz08@email.franklin.edu>
2023-02-25 22:10:19 -0500

    Remove backend

--- tf/main.tf
+++ tf/main.tf
@@ -8,13 +8,4 @@ terraform {
 
   required_version = "~> 1.3.6"
 
-  backend "s3" {
-    bucket = "personal-project-remote-state"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-2"
-
-    dynamodb_table = "personal-project-tf-statelock"
-    encrypt        = true
-  }
-
 }
